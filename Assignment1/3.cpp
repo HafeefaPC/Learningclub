83c300e742354f0c45e15d5bd85125d1bffc37e5
HafeefaPC <hafeefapc2003@gmail.com>
2023-09-11 07:42:26 +0530

    1.3

---
+++ Assignment1/3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int x,y;
+    cout << "Enter two numbers";
+    cin>>x>>y;
+    cout<<"The sum of two integers are "<<x+y;
+     return 0;
+}
